2ce2b5dcf7528c92cd94de4bfe63f33e4033c4bd
Akash Sharma <soulakash20002@gmail.com>
2024-11-30 09:44:18 +0530

    Navbar

---
+++ Web/Zentry/src/App.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import Hero from './components/Hero'
+import About from './components/About'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
+      <Hero />
+
+      {/* <section className="z-0 min-h-screen bg-blue-500" /> */}
+
+      <About />
+    </main>
+  )
+}
+
+export default App
