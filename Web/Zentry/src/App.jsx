32c934eefdeb49ac10db436a0466b7dce34e10c3
Akash Sharma <soulakash20002@gmail.com>
2024-11-30 11:45:58 +0530

    Features Section

--- Web/Zentry/src/App.jsx
+++ Web/Zentry/src/App.jsx
@@ -2,18 +2,20 @@
 import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
+import Features from "./components/Features.jsx";
 
 const App = () => {
-  return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Navbar />
-      <Hero />
+    return (
+        <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <Navbar />
+            <Hero />
 
-      {/* <section className="z-0 min-h-screen bg-blue-500" /> */}
+            {/* <section className="z-0 min-h-screen bg-blue-500" /> */}
 
-      <About />
-    </main>
-  )
+            <About />
+            <Features />
+        </main>
+    )
 }
 
 export default App
